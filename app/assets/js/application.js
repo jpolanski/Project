725e6e38c1d8ee62bb65a1e4365a808500e6e578
vinicius123 <vba321@hotmail.com>
2013-10-17 15:58:50 -0300

    Was made with an area hotel information such as daily, reservations, etc

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -1,12 +1,24 @@
+/*
+
 $(document).ready(function() {
   $('*[data-toggle="tooltip"]').tooltip()
   data_confirm();
 });
 
-/*** Confirm dialog **/
+/*** Confirm dialog 
 var data_confirm = function () {
    $('a[data-confirm]').click( function () {
       var msg = $(this).data('confirm');
       return confirm(msg);
    });
 };
+**/
+
+$(function() {
+    $( "#accordion" ).accordion();
+  });
+
+$(document).ready(function(){
+    $("#accordion").accordion();
+
+});
--- config/routes.php
+++ config/routes.php
@@ -3,8 +3,13 @@
     $router = new Router($_SERVER['REQUEST_URI']);
 
     $router->get('/', array('controller' => 'HomeController', 'action' => 'index'));
+
+    //Informações do hotel
     $router->get('/sobre', array('controller' => 'AboutController', 'action' => 'index'));
+
+    //Formulario de contato
     $router->get('/fale-conosco', array('controller' => 'ContactsController', 'action' => '_new'));
     $router->post('/fale-conosco',array('controller' => 'ContactsController' , 'action' => 'send'));
+
     $router->load();
 ?>
